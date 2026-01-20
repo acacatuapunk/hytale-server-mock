9a37c3e55e157b297453699ff88bd9af8610e68f
Lena Rodrigues <36284210+acacatuapunk@users.noreply.github.com>
2026-01-20 18:41:07 +0000

    Corrige order de requires no server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 
+const {Server} = require('socket.io');
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
